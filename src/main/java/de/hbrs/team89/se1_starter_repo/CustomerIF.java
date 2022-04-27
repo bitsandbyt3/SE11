27cfccc89475c44114cb41f0b8d3ed108078bb3a
Patrik Schumann <patrik.schumann@smail.inf.h-brs.de>
2022-04-27 12:03:53 +0200

    implemented Customer Interface

---
+++ src/main/java/de/hbrs/team89/se1_starter_repo/CustomerIF.java
@@ -0,0 +1,12 @@
+package de.hbrs.team89.se1_starter_repo;
+
+public interface CustomerIF {
+    String name();
+    double invoice();
+    int numberOfCars();
+    String[] numberplates();
+    void setName();
+    void getName();
+    void setNumberplates();
+    void getNumberplates();
+}
