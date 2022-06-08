b7be4774226cf7d6428f85de8d424bc668d8e080
schwanzuslongus2 <74601107+schwanzuslongus2@users.noreply.github.com>
2022-06-08 18:27:40 +0200

    servlets verschoben 2.0

---
+++ src/main/java/de/hbrs/team89/se1_starter_repo/Controller/LevelConstructorServ.java
@@ -0,0 +1,28 @@
+package de.hbrs.team89.se1_starter_repo.controller;
+
+public class LevelConstructorServ extends ParkhausServlet{
+    String name;
+    int maxParkingLots;
+
+    @Override
+    String NAME(){
+        return this.name;
+    }
+
+    @Override
+    int MAX(){ // maximum number of parking slots on level 1
+        return this.maxParkingLots;
+    }
+
+    @Override
+    String config(){
+        return ""; // use default config
+        // Config Format is "Max, open_from, open_to, delay, simulation_speed"
+        // e.g. return this.MAX() + ",5,23,100,10";  // TODO replace by your own parameters
+    }
+
+    public LevelConstructorServ(String name, int parkingSpaces) {
+        this.name = name;
+        this.maxParkingLots = parkingSpaces;
+    }
+}
