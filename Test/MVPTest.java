4fa05976a160daebfa5f28073d590f369342da1f
Patrik Schumann <patrik.schumann@smail.inf.h-brs.de>
2022-06-08 10:41:44 +0200

    leere Testhüllen erstellt

--- Test/MVPTest.java
+++ Test/MVPTest.java
@@ -2,21 +2,48 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import de.hbrs.team89.se1_starter_repo.CustomerIF;
 import de.hbrs.team89.se1_starter_repo.CorporateCustomer;
 import de.hbrs.team89.se1_starter_repo.Subscriber;
 
+import javax.swing.text.View;
+
 public class MVPTest {
+    View view;
+    Controller controller;
+    Model model;
 
 
     @BeforeEach
     void setUp(){
-
+        view = new View;
+        controller = new Controller;
+        model = new Models;
     }
     @AfterEach
     void tearDown(){
-
+        view = null;
+        controller = null;
+        model = null;
     }
+
+    @Test
+    void isControllerObject(){
+        assertTrue(controller instanceof Controller);
+    }
+
+    @Test
+    void isViewObject(){
+        assertTrue(view instanceof View);
+    }
+
+    @Test
+    void isModelObject(){
+        assertTrue(model instanceof Models);
+    }
+
     @Test
     void controllerCreateCar(){
 
