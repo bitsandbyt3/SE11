1509162bd58371df36dc33867e5c6e76747733e8
Patrik Schumann <patrik.schumann@smail.inf.h-brs.de>
2022-06-08 09:54:44 +0200

    leere Testhüllen erstellt

---
+++ Test/MVPTest.java
@@ -0,0 +1,40 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import de.hbrs.team89.se1_starter_repo.CustomerIF;
+import de.hbrs.team89.se1_starter_repo.CorporateCustomer;
+import de.hbrs.team89.se1_starter_repo.Subscriber;
+
+public class MVPTest {
+
+
+    @BeforeEach
+    void setUp(){
+
+    }
+    @AfterEach
+    void tearDown(){
+
+    }
+    @Test
+    void controllerCreateCar(){
+
+    }
+    @Test
+    void viewDailyIncome(){
+
+    }
+
+    @Test
+    void viewMonthlyIncome(){
+
+    }
+
+    @Test
+    void modelGetFreeParkingSpots(){
+
+    }
+
+
+}
---
+++ Test/TemplateTest.java
@@ -0,0 +1,37 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import de.hbrs.team89.se1_starter_repo.CustomerIF;
+import de.hbrs.team89.se1_starter_repo.CorporateCustomer;
+import de.hbrs.team89.se1_starter_repo.Subscriber;
+
+public class TemplateTest {
+
+    @BeforeEach
+    void setUp(){
+
+    }
+    @AfterEach
+    void tearDown(){
+
+    }
+    @Test
+    void templateSUM(){
+
+    }
+    @Test
+    void templateAVG(){
+
+    }
+
+    @Test
+    void templateEURO(){
+
+    }
+
+    @Test
+    void templateAVGDuration(){
+
+    }
+}
