d8f7067679a209e71c60d02a3dc1c4e596714191
Patrik Schumann <patrik.schumann@smail.inf.h-brs.de>
2022-07-05 17:25:29 +0200

    pipeline test

--- src/main/java/de/hbrs/team89/se1_starter_repo/Controlls.java
+++ src/main/java/de/hbrs/team89/se1_starter_repo/Controlls.java
@@ -2,7 +2,7 @@ package de.hbrs.team89.se1_starter_repo;
 
 
 import javax.servlet.annotation.WebServlet;
-
+//test
 @WebServlet(name = "controller", value = "/controller")
 public class Controlls {
 }
